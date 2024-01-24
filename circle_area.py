a21826b317e5ba2c127ac5db9ebdb51459e04eb6
Bob Colts <rcolts34@gmail.com>
2024-01-24 18:36:36 -0500

    Calculate average of two numbers / Area of Circle

---
+++ circle_area.py
@@ -0,0 +1,29 @@
+### Average of Two Numbers ###
+
+
+x = int(input("Please enter first number: "))
+
+print(x)
+
+y = int(input("Please enter second number: "))
+
+print(y)
+
+
+average = sum((x,y))/2
+
+print(average)
+
+
+
+### a. Area of a circle ###  -   A = πr2
+
+r = float(input("Please enter radius of circle: "))
+
+print(r)
+
+pi = 3.14
+
+area_of_circle = pi*(r**2)
+
+print(area_of_circle)
