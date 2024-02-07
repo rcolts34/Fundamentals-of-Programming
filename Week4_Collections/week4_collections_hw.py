b61dbd53f5226f99c5f2a991a251f8097b434a97
Bob Colts <rcolts34@gmail.com>
2024-02-07 16:55:56 -0500

    updating week 4 homework set changes

--- Week4_Collections/week4_collections_hw.py
+++ Week4_Collections/week4_collections_hw.py
@@ -1,11 +1,12 @@
 '''
+
 # 1. Access value 20 from the tuple. tuple1 = (“Car”, [34, 23, 8], False, [15, 20, 11])
 
-# a. Determine how many elements are in the tuple (4).
-# b. Determine what position value 20 is in (3).
-# c. Value 20 is part of another tuple. Determine how many elements in this tuple (3).
-# d. Determine what position value 20 is in (1).
-# e. Use both position values to access value 20.
+    # a. Determine how many elements are in the tuple (4).
+    # b. Determine what position value 20 is in (3).
+    # c. Value 20 is part of another tuple. Determine how many elements in this tuple (3).
+    # d. Determine what position value 20 is in (1).
+    # e. Use both position values to access value 20.
 
 tuple1 = ("Car", [34, 23, 8], False, [15, 20, 11])
 print(tuple1[3][1])
@@ -14,8 +15,8 @@ print(tuple1[3][1])
 
 # 2. Slice the last 3 elements of the given list. List1 = [44, 12, 578, 21, 134, 67]
 
-# a. If starting from the end (negative #s), one doesn't need to know how many elements are in the list.
-# b. -3 includes the 3rd to last element (start index), and an empty end index includes all elements after (in this case 2nd to last and last).
+    # a. If starting from the end (negative #s), one doesn't need to know how many elements are in the list.
+    # b. -3 includes the 3rd to last element (start index), and an empty end index includes all elements after (in this case 2nd to last and last).
 
 list1 = [44, 12, 578, 21, 134, 67]
 print(list1[-3:])
@@ -24,8 +25,8 @@ print(list1[-3:])
 
 # 3. Write a program to replace 20 with 200 in list1 = [5, 10, 15, 20, 75, 100, 50]. Try this approach: Use list1.index to get the position of 20. Then do list[position] = 200
 
-# a. Determine the position of value 20 (3) using (name.index(value)).  Set result to a variable.
-# b. Replace 20 with 200 by defining the name of the list and the position of value 20.  Set this equal to 200.
+    # a. Determine the position of value 20 (3) using (name.index(value)).  Set result to a variable.
+    # b. Replace 20 with 200 by defining the name of the list and the position of value 20.  Set this equal to 200.
 
 list1 = [5, 10, 15, 20, 75, 100, 50]
 list1_index = list1.index(20)
@@ -37,12 +38,12 @@ print(list1)
 
 # 4. Change the value of 33 to 66 in the given tuple. tuple1 = (11, [64, 33], 243, 123)
 
-# a. Convert tuple1 into a list (tuple's cannot be modified).
-# b. Set the list within the first list to a variable.
-# c. Find the position of the interior list.
-# d. Find the position of value 33.
-# e. Replace 33 with 66 by defining the name of the list and the position of value 20.  Set this equal to 200.
-# f. Convert the list back into tuple1.
+    # a. Convert tuple1 into a list (tuple's cannot be modified).
+    # b. Set the list within the first list to a variable.
+    # c. Find the position of the interior list.
+    # d. Find the position of value 33.
+    # e. Replace 33 with 66 by defining the name of the list and the position of value 20.  Set this equal to 200.
+    # f. Convert the list back into tuple1.
 
 tuple1 = (11, [64, 33], 243, 123)
 list1 = list(tuple1)
@@ -60,8 +61,8 @@ print(type(tuple1))
 
 # 5. Return a new set with unique items from both sets by removing duplicates.
 
-# a. Create a new set by joining the two sets (.union).
-# b. Sets cannot contain duplicates, so the new set will only contain unique values from both sets.
+    # a. Create a new set by joining the two sets (.union).
+    # b. Sets cannot contain duplicates, so the new set will only contain unique values from both sets.
 
 
 set1 = {10, 20, 30, 40, 50}
@@ -74,11 +75,11 @@ print(new_set)
 
 # 6. Create a new list by picking the odd-index items from the first list and even indexed items from the second list.
 
-# a. Extract odd index values from list1 by using an index range of (1:"empty").  This will start with index 1 and include the rest of the values.  Adding another range value of 2 will select every-other index (thus, 1,3,5,etc).
+    # a. Extract odd index values from list1 by using an index range of (1:"empty").  This will start with index 1 and include the rest of the values.  Adding another range value of 2 will select every-other index (thus, 1,3,5,etc).
 
-# b. Extract even index values from list2 the same way odd index values were extracted from list1 (start with index 0 instead of 1).
+    # b. Extract even index values from list2 the same way odd index values were extracted from list1 (start with index 0 instead of 1).
 
-# c. Create the new list by joining the two extracted lists together (list.extend).
+    # c. Create the new list by joining the two extracted lists together (list.extend).
 
 list1 = [3, 6, 9, 12, 15, 18, 21]
 list2 = [4, 8, 12, 16, 20, 24, 28]
@@ -95,13 +96,13 @@ print(new_list)
 
 # 7. Consider list1 = [34, 54, 67, 89, 11, 43, 94]. Write a program to:
 
-    ###       a. remove the item present at index 4
-    ###       b. add it to the 3rd position and at the end of the list.
+        ###       a. remove the item present at index 4
+        ###       b. add it to the 3rd position and at the end of the list.
 
-# a. Identify the value located at index 4 (11)
-# b. Remove the value located at index 4
-# c. Use insert to add it to the 3rd position in the list
-# d. use append to add it to the end of the list.
+    # a. Identify the value located at index 4 (11)
+    # b. Remove the value located at index 4
+    # c. Use insert to add it to the 3rd position in the list
+    # d. use append to add it to the end of the list.
 
 list1 = [34, 54, 67, 89, 11, 43, 94]
 print(list1)
@@ -117,12 +118,12 @@ print(list1)
 
 # 8. Write a program to add item 7000 after 6000 in the following Python List
 
-# a. "Peel" list1 by setting each nested list to a variable
-# b. Add 7000 after 6000 to list3
-# c. Remove original list3 values from list2
-# d. Insert new list3
-# e. Remove original list2 values from list1
-# f. Insert new list2
+    # a. "Peel" list1 by setting each nested list to a variable
+    # b. Add 7000 after 6000 to list3
+    # c. Remove original list3 values from list2
+    # d. Insert new list3
+    # e. Remove original list2 values from list1
+    # f. Insert new list2
 
 list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
 list2 = [300, 400, [5000, 6000], 500]
@@ -160,21 +161,32 @@ list2 = ["h", "i", "j"]
 list1[2][1][2].extend(list2)
 print(list1)
 
-'''
-
 
 
 #10. Reverse the given tuple.
 
-tuple2 = (40, 19, 234, 12, 10, 123)
+    # a. Convert tuple1 to a list
+    # b. Reverse the list
+    # c. Convert the list back to tuple1
 
-list2 = list(tuple2)
-print(type(list2))
-list2.sort(reverse=True)
-print(list2)
+tuple1 = (40, 19, 234, 12, 10, 123)
+
+list1 = list(tuple1)
+print(type(list1))
+print(list1)
+list1.reverse()
+print(list1)
+tuple1 = tuple(list1)
+print(tuple1)
+print(type(tuple1))
+
+'''
 
 
 #11. Print the value of key ‘history’ in the below dictionary
+
+    # a. Identify hierarchy dictionary (course ↓ student ↓ marks ↓ classes)
+    # b. Print value of "history" by drilling down through hierarchy
 
 dict1 = {
     "course": {
@@ -188,6 +200,7 @@ dict1 = {
     }
 }
 
+print(dict1["course"]["student"]["marks"]["history"])
 
 
 
