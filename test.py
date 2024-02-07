c26ebb1115844f86c1abeee524b07e142ab5d4c8
Bob Colts <rcolts34@gmail.com>
2024-02-07 06:21:37 -0500

    updating repo file structure

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print('Hello World!'
+list2 = [300, 400, [5000, 6000], 500]
+list3 = [5000, 6000]
+list3_new = [5000, 6000]
+list2_new = [300, 400, list3_new, 500]
+list1_new = [10, 20, list2_new, 30, 40]
+list3.append(7000)
+print(list3)
+print(list2)
+print(list1)
+list3_new = [5000, 6000]
+list2.insert(2, list3)
+print(list3)
+
+
+
+
+
