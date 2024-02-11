3dbbe15c92339f1fcf58fb72c668eee29a2ee56b
Bob Colts <rcolts34@gmail.com>
2024-02-10 19:54:13 -0500

    adding week5 hw set updates to test.py

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ else:
 
 print(f"You chose to go {left_right}, then {swim_wait}, and went through the {door_opt} door. {result} ")
 
-"""
+
 
 print("Welcome to Treasure Island. Your mission is to find the treasure")
 
@@ -67,3 +67,85 @@ else:
     print(f"You chose {door_opt}. {result3}.")
 
 
+
+
+import random
+
+# rand_num = random.random()  # always returns a value between 0 and 1
+# print(round(rand_num, 2))
+
+user_move = input("Pick your move - rock/paper/scissors: ")
+rand_num = round(random.random(), 2)
+comp_move = ""
+;
+if rand_num >= 0 and rand_num < 1/3:
+    comp_move = "rock"
+elif rand_num >= 1/3 and rand_num <2/3:
+    comp_move = "paper"
+else:
+    comp_move = "scissors"
+
+result = ""
+if user_move == comp_move:
+    result = "Tie."
+elif user_move == "rock":
+    if comp_move == "paper":
+        result = "You lose!"
+    else:
+        result = "You win!"
+elif user_move == "paper":
+    if comp_move == "scissors":
+        result = "You lose!"
+    else:
+        result = "You win!"
+elif user_move == "scissors":
+    if comp_move == "rock":
+        result = "You lose!"
+    else:
+        result = "You win!"
+
+print(f"You Picked {user_move}. Computer picked {comp_move}. {result}")
+
+"""
+
+
+
+print("Welcome to Treasure Island. Your mission is to find the treasure")
+
+result1 = ""
+result2 = ""
+result3 = ""
+
+left_right = input("Choose a direction. left or right?: ")
+
+if left_right == "left":
+    print(f"You chose to go {left_right}")
+else:
+    result1 = "You Fell into a hole. Game Over."
+    print(f"You chose to go {left_right}. {result1}")
+    quit()
+
+swim_wait = input(("Choose an option. swim or wait?: "))
+
+if swim_wait == "wait":
+    print(f"You chose to {swim_wait}")
+else:
+    result2 = "You Were Attacked by trout. Game Over."
+    print(f"You chose to {swim_wait}. {result2}")
+    quit()
+
+door_opt = input(("Choose a door. red, yellow, or blue?: "))
+
+if door_opt == "yellow":
+    result3 = "You win!"
+    print(f"You chose {door_opt}. {result3}")
+elif door_opt == "red":
+    result3 = "You Were Burned by fire. Game Over"
+    print(f"You chose {door_opt}. {result3}.")
+    quit()
+elif door_opt == "blue":
+    result3 = "You Were Eaten by beasts. Game Over"
+    print(f"You chose {door_opt}. {result3}.")
+else:
+    result3 = "Game Over."
+    print(f"You chose {door_opt}. {result3}.")
