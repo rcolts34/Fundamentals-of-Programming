bb99f64500d33584b19c4cb97e915b1d3ae76b2d
Bob Colts <rcolts34@gmail.com>
2024-02-12 23:51:33 -0500

    updating test.py and week6 loops

--- week6_iterations/loops.py
+++ week6_iterations/loops.py
@@ -82,11 +82,54 @@ for num in [1, 2, 3]:
     for letter in "abc":
         print(num, letter)
 
-'''
+
+
+#### PRINT THE FOLLOWING
+
+#   1                       i=1 ║ j = (1,2)  ║   1               ║ prints (1,1)
+#   1 2                     i=2 ║ j = (1,3)  ║   1 2             ║ prints (1,2)
+#   1 2 3                   i=3 ║ j = (1,4)  ║   1 2 3           ║ prints (1,3)
+#   1 2 3 4                                  ║   1 2 3 4         ║
+#   1 2 3 4 5                                ║   1 2 4 3 5       ║
 
 rows = (int(input("Enter the number of rows: ")))
 
-for i in range(1, rows +1):
+for i in range(1, rows + 1):
     for j in range(1, i + 1):
-        print(j, end="")
+        print(j, end=" ")
     print()
+
+
+
+#### PRINT THE FOLLOWING
+
+#   5 4 3 2 1
+#   4 3 2 1
+#   3 2 1
+#   2 1
+#   1
+
+
+rows = int(input("Enter the number of rows: "))
+
+for i in reversed(range(1, rows + 1)):
+    for j in reversed(range(1, i + 1)):
+        print(j, end=" ")
+    print()
+
+
+'''
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(rows, 0, -1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
+
+
+
+
+
+
+
