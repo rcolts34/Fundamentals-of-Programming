bbf0f4b965524805a6d39a79f85ff027e883f188
Bob Colts <rcolts34@gmail.com>
2024-02-16 10:37:38 -0500

    "Tested out methods from comments"
    Signed-off-by: Bob Colts <rcolts34@gmail.com>

--- Week4_Collections/Bob_week4_collections_hw_DrShaik_Comments.py
+++ Week4_Collections/Bob_week4_collections_hw_DrShaik_Comments.py
@@ -124,6 +124,17 @@ list1.append(removed_item)
 print("Modified list:", list1)
 '''
 
+list1 = [34, 54, 67, 89, 11, 43, 94]
+removed_item = list1.pop(4)
+print(list1)
+list1.insert(2, removed_item)
+print(list1)
+list1.append(removed_item)
+print("Modified list:", list1)
+
+
+
+
 # 8. Write a program to add item 7000 after 6000 in the following Python List
 
     # a. "Peel" list1 by setting each nested list to a variable
@@ -152,6 +163,7 @@ print(list1)
 
 #### Alternatively, append 7000 after the 2nd nested list (5000,6000)
 
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
 list1[2][2].append(7000)
 print(list1)
 
