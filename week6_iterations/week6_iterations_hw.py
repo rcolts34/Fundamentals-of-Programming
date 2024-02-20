a53d78448341c41eb0787dea5156ea7361e23a49
Bob Colts <rcolts34@gmail.com>
2024-02-20 13:40:48 -0500

    adding updates to week6 iterations homework set

--- week6_iterations/week6_iterations_hw.py
+++ week6_iterations/week6_iterations_hw.py
@@ -36,10 +36,14 @@ print(prime_list)
 ### 2. Write a program to print a list of all even numbers till a given target number
 ### Input:   10
 
-# The program checks if all nu
+# The program checks if all numbers from 0 to target number are even and appends them to even_list
+# even_list must be defined before the program (empty)
+# The range is defined as all numbers from 0 to the target number +1 (includes target number)
+# If the number is even, it is added to the even_list
+    # The program checks whether each number in the range is even by dividing by two (modulus).  If the remainder is zero, the number is not even, at which point the program moves on to the next number
 
 
-target_num = int(input("Enter a number: "))
+target_num = (int(input("Enter a number: ")))
 
 even_list = []
 
@@ -48,17 +52,18 @@ for num in range(0, target_num+1):
     for i in range(0, target_num):
         if num % 2 != 0:
             isEven = False
-            break
     if isEven is True:
         even_list.append(num)
 
-    print(even_list)
+print(even_list)
 
 
 
 ### 3. Use a loop to display elements from a given list present at odd index positions.
 ### Input :  [10, 20, 30, 40, 50, 60]
 
+# The range is as (1, len(list1), 2).  Because we want the odd indexed positions, we start at index 1, and using a step of 2, we print every value after
+# Using the length of the list as the top part of the range allows us to print all odd index positions regardless of length of list.
 
 list1 = [10, 20, 30, 40, 50, 60]
 
@@ -70,6 +75,9 @@ for num in range(1, len(list1), 2):
 """
 
 # 4. Find the highest number from a given list of numbers using for loop. Do not use any in-built functions
+
+# The program checks each value in the list and sets it equal to highest_value if it is higher than the current highest_value.
+# Because it is checking each value in the list, eventually the actual highest value in the list will remain equal to highest_value.
 
 list1 = [10, 24, 8, 187, 34, 100]
 
